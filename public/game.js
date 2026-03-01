353819846efcf9d18492e147b580cacd5ba358bd
mariomito250 <mariomito250@gmail.com>
2026-03-01 15:20:53 -0300

    Mostra vida dos jogadores na tela

--- public/game.js
+++ public/game.js
@@ -1,30 +1,29 @@
-const socket = io();
-let myTurn = false;
-let roomId = null;
-
-socket.emit("find_match");
-
-socket.on("start_game", data => {
-  roomId = data.room;
-  myTurn = data.turn === socket.id;
-  updateStatus();
-});
-
-socket.on("update", game => {
-  myTurn = game.turn === socket.id;
-  updateStatus(game);
-});
-
-socket.on("game_over", winner => {
-  alert(winner === socket.id ? "Você venceu!" : "Você perdeu!");
-});
-
-document.getElementById("attack").onclick = () => {
-  socket.emit("attack", { room: roomId });
-};
-
 function updateStatus(game) {
   document.getElementById("status").innerText =
     myTurn ? "Seu turno" : "Turno do oponente";
+
   document.getElementById("attack").disabled = !myTurn;
+
+  if (game) {
+    const players = game.players;
+    const myId = socket.id;
+    const enemyId = Object.keys(players).find(id => id !== myId);
+
+    document.getElementById("myHp").innerText = players[myId].hp;
+    document.getElementById("enemyHp").innerText = players[enemyId].hp;
+  }
+}(game) {
+  document.getElementById("status").innerText =
+    myTurn ? "Seu turno" : "Turno do oponente";
+
+  document.getElementById("attack").disabled = !myTurn;
+
+  if (game) {
+    const players = game.players;
+    const myId = socket.id;
+    const enemyId = Object.keys(players).find(id => id !== myId);
+
+    document.getElementById("myHp").innerText = players[myId].hp;
+    document.getElementById("enemyHp").innerText = players[enemyId].hp;
+  }
 }
